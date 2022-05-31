3b41f27b0f9a73c394c6c45093cdc55c8b78108f
Charlie Conneely <charlie.conneely@genesys.com>
2022-05-31 11:58:17 +0100

    Finished first draft of module

---
+++ main.tf
@@ -0,0 +1,31 @@
+resource "genesyscloud_integration_action" "action" {
+    name           = var.action_name
+    category       = var.action_category
+    integration_id = var.integration_id
+    secure         = var.secure_data_action
+    
+    contract_input  = jsonencode({
+        "additionalProperties" = true,
+        "properties" = {
+            "userId" = {
+                "type" = "string"
+            }
+        },
+        "type" = "object"
+    })
+    contract_output = jsonencode({
+        "additionalProperties" = true,
+        "properties" = {},
+        "type" = "object"
+    })
+    
+    config_request {
+        request_template     = "{}"
+        request_type         = "POST"
+        request_url_template = "/api/v2/users/$${input.userId}/invite"
+    }
+
+    config_response {
+        success_template = "$${rawResult}"
+    }
+}
